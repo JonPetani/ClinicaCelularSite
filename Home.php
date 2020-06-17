4929e8c311e9a015b01a007b21ab24f6c161af7c
JonPetani <49282269+JonPetani@users.noreply.github.com>
2020-06-16 22:04:23 -0400

    some stuff

---
+++ Home.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+<link href="CSS/main.css" rel="stylesheet"/>
+</head>
+<body>
+<nav align=center>
+<a href="">Home</a>
+<a href="">Tech Services</a>
+<a href="">Online Tech Shop</a>
+<a href="">About Us</a>
+<a href="">Contact Us</a>
+<a href="">Register Account</a>
+<a href="">Log In</a>
+</nav>
+<main>
+<h1>clínica celular</h1>
+<p>Welcome to our site.</p>
+</main>
+</body>
+</html>
---
+++ store.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+<link href="CSS/main.css" rel="stylesheet"/>
+</head>
+<body>
+<nav align=center>
+<a href="">Home</a>
+<a href="">Tech Services</a>
+<a href="">Online Tech Shop</a>
+<a href="">About Us</a>
+<a href="">Contact Us</a>
+<a href="">Register Account</a>
+<a href="">Log In</a>
+</nav>
+<main>
+<?php
+?>
+</main>
+</body>
+</html>
